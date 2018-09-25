68bdd0e1ff718d8e5b59b8331de43de8b1d6a0cc
bristola <bristola@allegheny.edu>
2018-09-24 23:06:51 -0400

    Added a lot of stuff

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import profile_pic from "../images/image1.png"
+import twitter from "../images/twitter.png"
+import github from "../images/github.png"
+import linkedin from "../images/linkedin.png"
 import { Link } from 'gatsby'
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: '#435777',
     }}
   >
     <div
@@ -13,6 +16,7 @@ const Header = ({ siteTitle }) => (
         margin: '0 auto',
         maxWidth: 960,
         padding: '1.45rem 1.0875rem',
+        alignItems: 'center',
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -23,9 +27,19 @@ const Header = ({ siteTitle }) => (
             textDecoration: 'none',
           }}
         >
-          {siteTitle}
+          Austin Bristol
         </Link>
       </h1>
+      <img src={profile_pic} alt="Profile Picture"
+        style={{
+            marginBottom: '10px',
+        }}/>
+      <div className="socials" style={{
+      }}>
+      <a href="https://www.linkedin.com/in/austinbristol/"><img className="socialIMG" src={linkedin} alt="LinkedIn"/></a>
+      <a href="https://github.com/bristola"><img className="socialIMG" src={github} alt="GitHub"/></a>
+      <a href="https://twitter.com/bristola64"><img className="socialIMG" src={twitter} alt="Twitter"/></a>
+      </div>
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,19 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi people testing to see if this works</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <Layout>
+        <ul>
+            <li><a className="active" href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Education</a></li>
+            <li><a href="">Experience</a></li>
+            <li><a href="">Projects</a></li>
+        </ul>
+        <h1>Hi people testing to see if this works</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        //<Link to="/page-2/">Go to page 2</Link>
+    </Layout>
 )
 
 export default IndexPage
