538ce705182345ddced6a91bb9be954fc11a2693
bristola <bristola@allegheny.edu>
2018-10-03 21:06:41 -0400

    Added better about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
             <Link to="/page-4/"><li className="tabs2"><a href="">Projects</a></li></Link>
         </ul>
         <h2>About Me</h2>
-        <p>Student Athlete at Allegheny College majoring in Computer Science, minoring in Economics and Mathematics, and playing varsity Baseball. Upon graduation in May 2019, I am interested in employment opportunities in software development.</p>
+        <p>Student Athlete at Allegheny College majoring in Computer Science, and minoring in Economics and Mathematics. Playing varsity Baseball at my school. Upon graduation in May 2019, I am interested in employment opportunities in any area of software development. Specific interests lie in big data, data analytics, and distributed computing; have completed projects in these fields and enjoy them a lot. Outside of computer science, I enjoy playing baseball for my college team and going on hikes.</p>
     </Layout>
 )
 
